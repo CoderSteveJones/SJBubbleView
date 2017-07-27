847de910ff07f925379e93f3b2f266b4be02feaa
SteveJones <benkong_ah@foxmail.com>
2017-07-27 10:10:24 +0800

    Commit project.

---
+++ SJBubbleView/SJBubbleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SJBubbleView
+//
+//  Created by 江奔 on 2017/7/27.
+//  Copyright © 2017年 TCGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
